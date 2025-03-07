1fe4a44421c3b8d53e12ad0278c067fb86544f16
Aayush Singh <98281650+singh-aayush@users.noreply.github.com>
2025-03-07 11:30:36 +0530

    Frontend folder

---
+++ backend-integrated_folder/src/controllers/userController.js
@@ -0,0 +1,11 @@
+const db = require("../config/db");
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    console.log("come");
+    const [users] = await db.execute("SELECT id, name, email, role FROM users");
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
